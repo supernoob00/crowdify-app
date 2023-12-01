8e7f8b181b3f612e940a7c25ac786a06e9eb6f6d
Sam Somerdin <sjsomerdin@gmail.com>
2023-12-01 15:38:31 -0500

    Fixed sql

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,8 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS campaign;
-DROP TABLE IF EXISTS campaign_manager;
-DROP TABLE IF EXISTS donation;
-DROP TABLE IF EXISTS spend_request;
-DROP TABLE IF EXISTS votes;
+DROP TABLE IF EXISTS users, campaign, campaign_manager, donation, spend_request, votes CASCADE;
 
-CREATE TABLE user (
+CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
@@ -32,11 +27,11 @@ CREATE TABLE campaign (
 CREATE TABLE campaign_manager (
     campaign_id integer,
     manager_id integer,
-    creator boolean DEFAULT true NOT NULL --TODO: add constraint only non-creator managers can be removed, also if creator already exists default FALSE
+    creator boolean DEFAULT true NOT NULL, --TODO: add constraint only non-creator managers can be removed, also if creator already exists default FALSE
 
     CONSTRAINT pk_campaign_manager PRIMARY KEY (campaign_id, manager_id),
     CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id),
-    CONSTRAINT fk_manager_user_id FOREIGN KEY (manager_id) REFERENCES user (user_id)
+    CONSTRAINT fk_manager_user_id FOREIGN KEY (manager_id) REFERENCES users (user_id)
 );
 
 CREATE TABLE donation (
@@ -46,22 +41,23 @@ CREATE TABLE donation (
     donation_amount integer NOT NULL, --TODO: add constraint max amount 500,000
     donation_date integer NOT NULL, --TODO: add constraint between campaign start/end dates
     donation_comment varchar(200),
-    donation_status varchar(20) --TODO: pending, approved, rejected statuses?
+    donation_status varchar(20), --TODO: pending, approved, rejected statuses?
 
     CONSTRAINT pk_donation_id PRIMARY KEY (donation_id),
-    CONSTRAINT fk_campaign_id FOREIGN KEY campaign_id REFERENCES campaign (campaign_id)
+    CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id),
+    CONSTRAINT fk_donor_user_id FOREIGN KEY (donor_id) REFERENCES users (user_id)
 );
 
 CREATE TABLE spend_request (
     request_id SERIAL,
     campaign_id integer NOT NULL,
-    amount integer NOT NULL --TODO: constraint less than current funds - all donations minus all spend requests
+    amount integer NOT NULL, --TODO: constraint less than current funds - all donations minus all spend requests
     request_description varchar(500) NOT NULL,
     approved boolean DEFAULT false NOT NULL, --TODO: ask Jennifer if manager needs to manually approved spend requests, also add constraint cannot be true if majority has not approved
     end_date date NOT NULL, --TODO: Ask Jennifer is end date needed
 
     CONSTRAINT pk_request_id PRIMARY KEY (request_id),
-    CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id);
+    CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id)
 );
 
 CREATE TABLE vote (
@@ -69,9 +65,9 @@ CREATE TABLE vote (
     request_id integer NOT NULL,
     approved boolean, --TODO: ask jennifer is users can reject spend requests, also default null
 
-    CONSTRAINT pk_donor_request_id PRIMARY KEY (donor_id, request_id);
-    CONSTRAINT fk_donor_user_id (donor_id) REFERENCES user (user_id),
-    CONSTRAINT fk_request_id (request_id) REFERENCES spend_request (request_id),
+    CONSTRAINT pk_donor_request_id PRIMARY KEY (donor_id, request_id),
+    CONSTRAINT fk_donor_user_id FOREIGN KEY (donor_id) REFERENCES users (user_id),
+    CONSTRAINT fk_request_id FOREIGN KEY (request_id) REFERENCES spend_request (request_id)
 );
 
 COMMIT TRANSACTION;
