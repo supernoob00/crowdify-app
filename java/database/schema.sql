ff667f72587552719240fded09fcbf29591bbe68
Sam Somerdin <sjsomerdin@gmail.com>
2023-12-02 14:32:53 -0500

    Updated SQL schema, also added to test data script

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -3,10 +3,11 @@ CREATE FUNCTION check_campaign_single_creator() RETURNS trigger AS $check_single
         invalid_counts integer;
 
         --check if any campaign has more or less than one creator
-        SELECT COUNT creator
+        SELECT COUNT (campaign_id)
+        FROM
+        (SELECT campaign_id, COUNT (creator) AS creator_count FROM campaign_manager WHERE creator GROUP BY campaign_id) as counts
         INTO invalid_counts
-        FROM (COUNT creator FROM campaign_manager WHERE creator GROUP BY campaign)
-        WHERE creator != 1;
+        WHERE counts.creator_count != 1;
 
         IF invalid_counts > 0 THEN
             RAISE EXCEPTION 'Every campaign must have exactly one creator.';
@@ -15,8 +16,87 @@ CREATE FUNCTION check_campaign_single_creator() RETURNS trigger AS $check_single
     END;
 $check_single_creator$ LANGUAGE plpgsql;
 
+--TODO: needs to be tested
+CREATE FUNCTION donation_date_between_start_end_dates() RETURNS trigger AS $check_donation_date$
+    BEGIN
+        IF NOT NEW.donation_date BETWEEN (SELECT (start_date, end_date) FROM campaign WHERE campaign.campaign_id = NEW.campaign_id)
+            RAISE EXCEPTION 'Donation date must be between start and end dates of campaign.';
+        END IF;
+        RETURN NEW;
+    END;
+$check_donation_date$ LANGUAGE plpgsql;
+
+--TODO: needs to be tested
+CREATE FUNCTION spend_request_approved_only_with_majority_vote() RETURNS trigger AS $check_request_approved$
+    BEGIN
+        approve_votes integer;
+        total_votes integer;
+
+        SELECT COUNT (donor_id)
+        INTO approve_votes
+        FROM vote
+        WHERE vote.request_id = NEW.request_id AND vote_approved;
+
+        SELECT COUNT (donor_id)
+        INTO total_votes
+        FROM vote
+        WHERE vote.request_id = NEW.request_id;
+
+        IF NEW.approved AND (2 * approve_votes <= total_votes)
+            RAISE EXCEPTION 'A spend request cannot be approved if there''s not a majority vote';
+        END IF;
+        RETURN NEW;
+    END;
+$check_request_approved$ LANGUAGE plpgsql;
+
+CREATE FUNCTION only_non_manager_donors_vote_spend_request() RETURNS trigger AS $check_voters$
+    BEGIN
+        manager_voter_count integer;
+        non_donor_voter_count integer;
+
+        --count of voters for spend request who are a manager of the associated campaign
+        SELECT COUNT (DISTINCT donor_id)
+        FROM vote
+        INTO manager_voter_count
+        INNER JOIN spend_request USING (request_id)
+        INNER JOIN campaign_manager
+        ON (vote.donor_id = campaign_manager.manager_id)
+        AND (spend_request.campaign_id = campaign_manager.campaign_id);
+
+        IF manager_voter_count > 0
+            RAISE EXCEPTION 'A manager cannot vote for a spend request of their own campaign.';
+        END IF;
+
+        --count of voters for spend request who are not a donor
+        SELECT COUNT (vote.donor_id)
+        FROM vote
+        INTO non_donor_voter_count
+        INNER JOIN spend_request using (request_id)
+        LEFT JOIN donation
+        ON (vote.donor_id = donation.donor_id)
+        AND (spend_request.campaign_id = donation.campaign_id)
+        WHERE (donation.donation_id IS null);
+
+        IF non_donor_voter_count > 0
+            RAISE EXCEPTION 'Only donors to a campaign can vote for that campaign''s spend requests.';
+        END IF;
+        RETURN NEW;
+    END;
+$check_voters$ LANGUAGE plpgsql;
+
 CREATE TRIGGER check_campaign_single_creator BEFORE INSERT OR UPDATE OR DELETE ON campaign_manager
     EXECUTE PROCEDURE check_campaign_single_creator();
+
+CREATE TRIGGER donation_date_between_start_end_dates BEFORE INSERT OR UPDATE ON donation
+    FOR EACH ROW EXECUTE PROCEDURE donation_date_between_start_end_dates();
+
+CREATE TRIGGER spend_request_approved_only_with_majority_vote BEFORE INSERT OR UPDATE on spend_request
+    FOR EACH ROW EXECUTE PROCEDURE spend_request_approved_only_with_majority_vote();
+
+CREATE TRIGGER only_non_manager_donors_vote_spend_request BEFORE INSERT OR UPDATE on vote
+    EXECUTE PROCEDURE only_non_manager_donors_vote_spend_request();
+
+--- *** ---
 
 BEGIN TRANSACTION;
 
@@ -57,7 +137,6 @@ CREATE TABLE campaign_manager (
     CONSTRAINT pk_campaign_manager PRIMARY KEY (campaign_id, manager_id),
     CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id),
     CONSTRAINT fk_manager_user_id FOREIGN KEY (manager_id) REFERENCES users (user_id)
-    --TODO: add constraint for single creator (should use trigger)
 );
 
 CREATE TABLE donation (
@@ -72,14 +151,7 @@ CREATE TABLE donation (
     CONSTRAINT pk_donation_id PRIMARY KEY (donation_id),
     CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id),
     CONSTRAINT fk_donor_user_id FOREIGN KEY (donor_id) REFERENCES users (user_id),
-    CONSTRAINT valid_donation_amount CHECK (donation_amount > 0 AND donation_amount <= 50000000),
-    CONSTRAINT donation_date_between_start_end_dates
-        CHECK
-        (
-        donation_date
-        BETWEEN
-        (SELECT (start_date, end_date) FROM campaign WHERE campaign.campaign_id = donation.campaign_id)
-        )
+    CONSTRAINT valid_donation_amount CHECK (donation_amount > 0 AND donation_amount <= 50000000)
 );
 
 CREATE TABLE spend_request (
@@ -88,19 +160,10 @@ CREATE TABLE spend_request (
     request_amount integer NOT NULL, --TODO: constraint less than current funds - all donations minus all spend requests (should use trigger)
     request_description varchar(500) NOT NULL,
     request_approved boolean DEFAULT false NOT NULL, --TODO: ask Jennifer if manager needs to manually approved spend requests, also add constraint cannot be true if majority has not approved
-    end_date date NOT NULL, --TODO: Ask Jennifer is end date needed
+    end_date timestamp, --TODO: Ask Jennifer is end date needed
 
     CONSTRAINT pk_request_id PRIMARY KEY (request_id),
-    CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id),
-    CONSTRAINT approved_only_with_majority_vote
-        CHECK
-        (
-        (NOT approved)
-        OR
-        (SELECT COUNT donor_id FROM vote WHERE vote.request_id = spend_request.request_id AND vote_approved)
-        >
-        ((SELECT COUNT donor_id FROM vote WHERE vote.request_id = spend_request.request_id) / 2.0)
-        )
+    CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id)
 );
 
 CREATE TABLE vote (
@@ -110,12 +173,7 @@ CREATE TABLE vote (
 
     CONSTRAINT pk_donor_request_id PRIMARY KEY (donor_id, request_id),
     CONSTRAINT fk_donor_user_id FOREIGN KEY (donor_id) REFERENCES users (user_id),
-    CONSTRAINT fk_request_id FOREIGN KEY (request_id) REFERENCES spend_request (request_id),
-    CONSTRAINT no_manager_vote
-        CHECK
-        (
-        SELECT COUNT donor_id FROM vote INNER JOIN campaign_manager ON (donor_id = manager_id) = 0
-        )
+    CONSTRAINT fk_request_id FOREIGN KEY (request_id) REFERENCES spend_request (request_id)
 );
 
 COMMIT TRANSACTION;
