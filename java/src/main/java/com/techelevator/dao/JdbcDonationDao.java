2fd943d302a0cbca5ce6028ded600fa01fc81002
Chad Vogler <cdvogler@gmail.com>
2023-12-02 16:09:22 -0500

    updates to Donation Controller/Dao/Dto

--- java/src/main/java/com/techelevator/dao/JdbcDonationDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcDonationDao.java
@@ -1,7 +1,11 @@
 package com.techelevator.dao;
 
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Donation;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +23,7 @@ public class JdbcDonationDao {
     public List<Donation> donationList(int userId) {
         List<Donation> donationList = new ArrayList<>();
 
-       // String sql =
+        // String sql =
 
         return null;
     }
@@ -30,8 +34,70 @@ public class JdbcDonationDao {
 
     // make a new donation to a campaign
 
+    public Donation createDonation(Donation donationToCreate) {
+        String sql = "INSERT into donation " +
+                "(donor_id, campaign_id, donation_amount, donation_date, donation_comment, donation_status " +
+                "values (?,?,?,?,?,?) returning donation_id;";
+
+        try {
+            int donationId = jdbcTemplate.queryForObject(sql, Integer.class,
+                    // TODO same question about donor id
+                    donationToCreate.getDonor().getId(),
+                    donationToCreate.getCampaignId(),
+                    donationToCreate.getAmount(),
+                    donationToCreate.getDate(),
+                    donationToCreate.getComment(),
+                    donationToCreate.getStatus());
+            return getDonationById(donationId);
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+    }
+
+
+  /*
 
 
 
 
+
+*/
+
+    // get donation by Id
+
+    public Donation getDonationById(int id) {
+        String sql = "SELECT * FROM donation WHERE donation_id = ?;";
+
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+
+            if (results.next()) {
+                Donation retrievedDonation = mapRowToTransfer(results);
+                return retrievedDonation;
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+        return null;
+
+    }
+
+
+    public Donation mapRowToTransfer(SqlRowSet results) {
+        Donation donation = new Donation();
+
+        donation.setDonationId(results.getInt("donation_id"));
+        // donor id?
+        donation.setCampaignId(results.getInt("campaign_id"));
+        donation.setAmount(results.getInt("donation_amount"));
+        donation.setDate(results.getTimestamp("donation_date").toLocalDateTime());
+        donation.setComment(results.getString("donation_comment"));
+        donation.setStatus(results.getString("donation_status"));
+
+        return donation;
+    }
 }
---
+++ java/src/main/java/com/techelevator/model/DonationDto.java
@@ -0,0 +1,74 @@
+package com.techelevator.model;
+
+import java.sql.Timestamp;
+
+public class DonationDto {
+
+    private int donorId;
+    private int campaignId;
+    private int donationAmount;
+    private Timestamp donationDate;
+    private String donationComment;
+    private String donationStatus;
+
+    public DonationDto() {
+    }
+
+    public DonationDto(int donorId, int campaignId, int donationAmount, Timestamp donationDate, String donationComment, String donationStatus) {
+        this.donorId = donorId;
+        this.campaignId = campaignId;
+        this.donationAmount = donationAmount;
+        this.donationDate = donationDate;
+        this.donationComment = donationComment;
+        this.donationStatus = donationStatus;
+
+    }
+
+    public int getDonorId() {
+        return donorId;
+    }
+
+    public void setDonorId(int donorId) {
+        this.donorId = donorId;
+    }
+
+    public int getCampaignId() {
+        return campaignId;
+    }
+
+    public void setCampaignId(int campaignId) {
+        this.campaignId = campaignId;
+    }
+
+    public int getDonationAmount() {
+        return donationAmount;
+    }
+
+    public void setDonationAmount(int donationAmount) {
+        this.donationAmount = donationAmount;
+    }
+
+    public Timestamp getDonationDate() {
+        return donationDate;
+    }
+
+    public void setDonationDate(Timestamp donationDate) {
+        this.donationDate = donationDate;
+    }
+
+    public String getDonationComment() {
+        return donationComment;
+    }
+
+    public void setDonationComment(String donationComment) {
+        this.donationComment = donationComment;
+    }
+
+    public String getDonationStatus() {
+        return donationStatus;
+    }
+
+    public void setDonationStatus(String donationStatus) {
+        this.donationStatus = donationStatus;
+    }
+}
