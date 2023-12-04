5e4af92496c5f3abea508e2a369b5cd2694f37c5
Sam Somerdin <sjsomerdin@gmail.com>
2023-12-04 12:28:58 -0500

    All tests now pass

--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -11,6 +11,10 @@ VALUES ('Fancy New Robot', 'We are making some cool new stuff using a cool robot
 INSERT INTO campaign (campaign_name, description, funding_goal, start_date, end_date, locked, public) -- id 3
 VALUES ('Fancy New Computer', 'We are making some cool new stuff using a quantum computer', 3000000, '2024-1-3', '2024-1-23', true, false);
 
+INSERT INTO campaign_manager VALUES (1, 1, true);
+INSERT INTO campaign_manager VALUES (2, 2, true);
+INSERT INTO campaign_manager VALUES (3, 3, true);
+
 INSERT INTO donation (donor_id, campaign_id, donation_amount, donation_date, donation_comment)
 VALUES (1, 1, 1000, '2024-1-11', 'Nice stuff with Tech!'); -- id 1
 INSERT INTO donation (donor_id, campaign_id, donation_amount, donation_date, donation_comment)
