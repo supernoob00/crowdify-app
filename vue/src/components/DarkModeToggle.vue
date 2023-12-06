3da6947f5a2556e882efc6560ac3f796c73c577d
Aditya Suresh[D[D[D[D[D[D[D[D[C[F[B <adityasuresh25@gmail.com>
2023-12-06 12:48:38 -0500

    extra thing

--- vue/src/components/DarkModeToggle.vue
+++ vue/src/components/DarkModeToggle.vue
@@ -27,11 +27,11 @@ export default {
   methods: {
     switchTheme(e: Event) {
       if ((<HTMLInputElement>e.target).checked) {
-        // localStorage.setItem('theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         document.documentElement.setAttribute('data-theme', 'dark');
         this.isLight = false;
       } else {
-        // localStorage.setItem('theme', 'light');
+        localStorage.setItem('theme', 'light');
         document.documentElement.setAttribute('data-theme', 'light');
         this.isLight = true;
       }
