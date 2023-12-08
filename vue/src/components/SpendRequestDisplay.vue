4a7566efb9bf747e7bf377952f6eb8aed70bcd9d
Aditya Suresh[D[D[D[D[D[D[D[D[C[F[B <adityasuresh25@gmail.com>
2023-12-08 11:43:14 -0500

    pulled updates

---
+++ vue/src/components/SpendRequestDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ spendRequest.name }}
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['spendRequest']);
+
+</script>
+
+<style scoped></style>
