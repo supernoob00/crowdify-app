35ed0d7282b51e0e2ae6950bc480ab2fe257d997
Josh Mankey <jmank38@gmail.com>
2023-12-11 14:26:00 -0500

    Fixed some tests

--- java/src/test/java/com/techelevator/dao/BaseDaoTests.java
+++ java/src/test/java/com/techelevator/dao/BaseDaoTests.java
@@ -18,32 +18,51 @@ public abstract class BaseDaoTests {
     protected static final User USER_1 = new User(1, "user1", "user1@email.com", "user1", "ROLE_USER");
     protected static final User USER_2 = new User(2, "user2", "user2@email.com", "user2", "ROLE_USER");
     protected static final User USER_3 = new User(3, "user3", "user3@email.com", "user3", "ROLE_USER");
-    protected static final Donation DONATION_1 = new Donation(1, USER_1, 1,
-            "Fancy New Tech", 1000,
-            LocalDateTime.of(2024, 1, 11, 0, 0), "Nice stuff with Tech!",
-            false, true);
-    protected static final Donation DONATION_2 = new Donation(2, USER_2, 2,
-            "Fancy New Robot", 2000,
-            LocalDateTime.of(2024, 1, 12, 0, 0), "Go Robots!", false, true);
-    protected static final Donation DONATION_3 = new Donation(3, USER_3, 3,
-            "Fancy New Computer", 3000,
-            LocalDateTime.of(2024, 1, 13, 0, 0), "What the frick does Quantum" +
-            " mean??", false, false);
-    protected static final Donation DONATION_4 = new Donation(4, USER_1, 2,
-            "Fancy New Robot", 2000,
-            LocalDateTime.of(2024, 1, 11, 0, 0), "Not bad!", false, true);
-    protected static final Campaign CAMPAIGN_1 = new Campaign(1, "Fancy New Tech", "We are making some cool new stuff using technology",
+    protected static final User USER_4 = new User(4, "user4", "user4@email.com", "user4", "ROLE_USER");
+    protected static final User USER_5 = new User(5, "user5", "user5@email.com", "user5", "ROLE_USER");
+
+    protected static final Campaign CAMPAIGN_1 = new Campaign(
+            1, "Fancy New Tech", "We are making some cool new stuff using technology",
             1000000, LocalDateTime.of(2024, 1, 1, 0, 0),
-            LocalDateTime.of(2024, 1, 21, 0, 0), false, false, USER_1, false);
-    protected static final Campaign CAMPAIGN_2 = new Campaign(2, "Fancy New Robot", "We are making some cool new stuff using a cool robot",
+            LocalDateTime.of(2024, 1, 21, 0, 0),
+            false, false, USER_1, false);
+    protected static final Campaign CAMPAIGN_2 = new Campaign(
+            2, "Fancy New Robot", "We are making some cool new stuff using a cool robot",
             2000000, LocalDateTime.of(2024, 1, 2, 0, 0),
-            LocalDateTime.of(2024, 1, 22, 0, 0), false, true, USER_2, false);
-    protected static final Campaign CAMPAIGN_3 = new Campaign(3, "Fancy New Computer", "We are making some cool new stuff using a quantum computer",
+            LocalDateTime.of(2024, 1, 22, 0, 0),
+            false, true, USER_2, false);
+    protected static final Campaign CAMPAIGN_3 = new Campaign(
+            3, "Fancy New Computer", "We are making some cool new stuff using a quantum computer",
             3000000, LocalDateTime.of(2024, 1, 3, 0, 0),
-            LocalDateTime.of(2024, 1, 23, 0, 0), true, false, USER_3, false);
-    protected static final Campaign CAMPAIGN_4 = new Campaign(4, "Doomed to " +
-            "Fail", "Why do I even try", 100, LocalDateTime.of(2024, 1, 3, 0, 0),
-            LocalDateTime.of(2024, 1, 23, 0, 0), true, false, USER_1, true);
+            LocalDateTime.of(2024, 1, 23, 0, 0),
+            true, false, USER_3, false);
+    protected static final Campaign CAMPAIGN_4 = new Campaign(
+            4, "Doomed to Fail", "Why do I even try",
+            4000000, LocalDateTime.of(2024, 1, 3, 0, 0),
+            LocalDateTime.of(2024, 1, 23, 0, 0),
+            true, false, USER_1, true);
+
+
+    protected static final Donation DONATION_1 = new Donation(
+            1, USER_1, 1, "Fancy New Tech", 1000,
+            LocalDateTime.of(2024, 1, 11, 0, 0),
+            "Nice stuff with Tech!", false, true);
+    protected static final Donation DONATION_2 = new Donation(
+            2, USER_2, 2, "Fancy New Robot", 2000,
+            LocalDateTime.of(2024, 1, 12, 0, 0),
+            "Go Robots!", false, true);
+    protected static final Donation DONATION_3 = new Donation(
+            3, USER_3, 3, "Fancy New Computer", 3000,
+            LocalDateTime.of(2024, 1, 13, 0, 0),
+            "What the frick does Quantum mean??", false, false);
+    protected static final Donation DONATION_4 = new Donation(
+            4, USER_1, 2, "Fancy New Robot", 2000,
+            LocalDateTime.of(2024, 1, 11, 0, 0),
+            "Not bad!", false, true);
+    protected static final  Donation DONATION_5 = new Donation(
+            5, USER_5,1, "Fancy New Tech", 1000,
+            LocalDateTime.of(2024, 1,15, 0, 0),
+            "Nice!", false, false);
 
     protected static final SpendRequest REQUEST_1 = new SpendRequest(1, 1, "More Money", 5000, "Extra Awesome Tech Stuff", false,
             LocalDateTime.of(2024, 1, 24, 0,0));
