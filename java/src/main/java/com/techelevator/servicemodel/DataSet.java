57936874e8e90be70dc1a982fdc90001dcba38c6
Chad Vogler <cdvogler@gmail.com>
2023-12-11 19:50:25 -0500

    Added skeleton of chart API functionality.

---
+++ java/src/main/java/com/techelevator/servicemodel/DataSet.java
@@ -0,0 +1,39 @@
+package com.techelevator.servicemodel;
+
+import java.util.List;
+
+public class DataSet {
+    private List<Integer> data;
+    private List<String> backgroundColor;
+    private String label;
+
+    public DataSet(List<Integer> data, List<String> backgroundColor, String label) {
+        this.data = data;
+        this.backgroundColor = backgroundColor;
+        this.label = label;
+    }
+
+    public List<Integer> getData() {
+        return data;
+    }
+
+    public void setData(List<Integer> data) {
+        this.data = data;
+    }
+
+    public List<String> getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(List<String> backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
