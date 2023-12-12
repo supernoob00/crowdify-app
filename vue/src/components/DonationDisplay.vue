0a0777df7f479d554136a04e781bf7fc7fa582cf
Sam Somerdin <sjsomerdin@gmail.com>
2023-12-12 16:33:45 -0500

    stuf

--- vue/src/components/DonationDisplay.vue
+++ vue/src/components/DonationDisplay.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="message-body">
       <span id="amount">${{ donationAmount }}</span>
-      <p class="title is-6" id="comment">{{ donation.comment }}</p>
+      <p class="title is-6 mt-3" id="comment">{{ donation.comment }}</p>
     </div>
   </article>
 </template>
